f9803c532355a7673d647cbbdbcb16f3e3d7ef82
Jaco Wang（狗宝） <jaco.wang@basebit.ai>
2022-11-14 03:18:25 +0000

    cert chain

--- main.go
+++ main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
-	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"time"
 
 	"github.com/digitalocean/go-qemu/qmp"
 )
+
+type AttestReport struct {
+	// Returns     string `json:"id"`
+	Return struct {
+		Data string `json:"data"`
+	} `json:"return"`
+}
+
+type capabilities struct {
+	Return struct {
+		PDH       string `json:"pdh"`
+		CertChain string `json:"cert-chain"`
+		CpuId     string `json:"cpu0-id"`
+		Cbit      int    `json:"cbitpos"`
+		Reduced   int    `json:"reduced-phys-bits"`
+	} `json:"return"`
+}
 
 func main() {
 	monitor, err := qmp.NewSocketMonitor("tcp", "10.0.2.2:4444", 2*time.Second)
@@ -16,7 +33,18 @@ func main() {
 	monitor.Connect()
 	defer monitor.Disconnect()
 
-	cmd := []byte(`{"execute":"query-sev-attestation-report","arguments":{"mnonce":"ZBaOEOsVmenc5q34VJb9jw=="}}`)
+	// cmd := []byte(`{"execute":"query-sev-attestation-report","arguments":{"mnonce":"ZBaOEOsVmenc5q34VJb9jw=="}}`)
+	cmd := []byte(`{"execute":"query-sev-capabilities"}`)
 	raw, _ := monitor.Run(cmd)
-	fmt.Println(base64.StdEncoding.EncodeToString(raw))
+	var c capabilities
+	json.Unmarshal(raw, &c)
+	fmt.Println(c.Return.CertChain)
+	//		mnonce, _ := base64.StdEncoding.DecodeString("ZBaOEOsVmenc5q34VJb9jw==")
+	//		data, _ := base64.StdEncoding.DecodeString("eyJyZXR1cm4iOiB7ImRhdGEiOiAiWkJhT0VPc1ZtZW5jNXEzNFZKYjlqK2tjVFl0WGZHbDVPcWFVODFWN1VmVzVSbmhXUmJ5Wi9WOHI0Z3lEVGE5UkFRQUFBQUlRQUFBQ0FBQUFBQUFBQUpCNmh5ck9tZUpjZWZtTzJMV2ZEcy8yeGs5eUM4bklyMHU2cEZ3YlNOcFpzeTExYUg5Tk05OWY4MGI1NHRxSWdnQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUM4aUVzOUdQZ0pkcU8weTNIWnN6OGdUYkFLQ2hHWFRmR1RpaTl0ZjgvYmVKTTE5TXovdUE3SVZVcmlkYTMySFVBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUE9PSJ9fQ==")
+	//		var attestReport AttestReport
+	//		json.Unmarshal(data, &attestReport)
+	//		fmt.Println(mnonce)
+	//		d, _ := base64.StdEncoding.DecodeString(attestReport.Return.Data)
+	//		fmt.Println(d[0:0x10])
+	//	}
 }
